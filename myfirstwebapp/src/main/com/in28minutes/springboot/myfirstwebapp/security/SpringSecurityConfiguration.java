d7b904aff10f50078746dca94912ffca2f03d47c
ParitoshDadhich <pdadhich0481@gmail.com>
2022-12-19 03:49:50 +0530

    removed withDefaultPasswordEncoder() method and add PasswordEncoder

--- myfirstwebapp/src/main/com/in28minutes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
+++ myfirstwebapp/src/main/com/in28minutes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
@@ -1,9 +1,13 @@
 package com.in28minutes.springboot.myfirstwebapp.security;
 
+import java.util.function.Function;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
@@ -13,12 +17,26 @@ public class SpringSecurityConfiguration {
 	
 	@Bean
 	public InMemoryUserDetailsManager createUserDetailsManager() {
-		UserDetails userDetails = User.withDefaultPasswordEncoder()
-		.username("Paritosh Dadhich")
-		.password("pdadhich")
-		.roles("USER", "ADMIN")
-		.build();
-		
+		Function<String, String> passwordEncoder = 
+				input -> passwordEncoder().encode(input);
+				
+		UserDetails userDetails = User.builder()
+									.passwordEncoder(passwordEncoder)
+									.username("Paritosh Dadhich")
+									.password("pdadhich")
+									.roles("USER", "ADMIN")
+									.build();
+									
 		return new InMemoryUserDetailsManager(userDetails);
+	}
+	
+	/*
+	 * Earlier was using withDefaultPasswordEncoder() -> that was a deprecated function, that is not good for production
+	 * now will be using PasswordEncoder which is safe and used strong hash function
+	 */
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
